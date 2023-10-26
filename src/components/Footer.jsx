cba169d45ae4d54a71158d5ac745b98e3d0abfdd
Todd H. Albert <todd@bocacode.com>
2023-10-26 11:55:05 -0400

    framed out components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Todd Albert. All rights reserved.
+      </p>
+    </footer>
+  )
+}
